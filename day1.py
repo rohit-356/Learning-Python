150cebcab6f9fe96aaf0d578f8c9b007aaf2167a
rohit-356 <rohitsharma88882@gmail.com>
2026-01-15 21:35:50 -0800

    basic calculator

---
+++ day1.py
@@ -0,0 +1,11 @@
+A = int(input("Enter a number: "))
+B = int(input("Enter another number: "))
+c = input("enter operator: ")
+if c == "+":
+    print("The sum of", A, "and", B, "is", A + B)
+elif c == "-":
+    print("The difference of", A, "and", B, "is", A - B)
+elif c == "*":
+    print("The product of", A, "and", B, "is", A * B)
+elif c == "/":
+    print("The quotient of", A, "and", B, "is", A / B)
